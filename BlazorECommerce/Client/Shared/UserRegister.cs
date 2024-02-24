c038ee2fc90cd526394646c48bccf4e0cd9ab2d3
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2024-02-24 18:55:16 +0300

    Add the UserRegister Model

---
+++ BlazorECommerce/Client/Shared/UserRegister.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorECommerce.Client.Shared
+{
+    public class UserRegister
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
